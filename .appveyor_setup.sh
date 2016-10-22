8cc66602122cd62f1f82b5ecde00a1703f26b601
Nicholas Hutchinson <nshutchinson@gmail.com>
2016-10-23 00:29:57 +0100

    CI: Appveyor - general tidy; build with Ninja, not msbuild
    Though this does mean we need to download and install it on each run, it
    is significantly less verbose than msbuild and allows uniformity between
    different CI configs.

--- .appveyor_setup.sh
+++ .appveyor_setup.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 set -euo pipefail
+set -o xtrace
 
-if [[ ! -d "_deps/boost_1_56_0-msvc-10.0-64" ]]; then
-    set -o xtrace
-
+if ! type innoextract >/dev/null 2>&1; then
     mkdir -p _deps
     pushd _deps
-    cinst innoextract
+    curl -fLO http://constexpr.org/innoextract/files/innoextract-1.6-windows.zip
+    7z x innoextract-1.6-windows.zip -obin innoextract.exe
+    innoextract --version
+    rm innoextract-1.6-windows.zip
+    popd
+fi
+
+if ! type ninja >/dev/null 2>&1; then
+    mkdir -p _deps
+    pushd _deps
+    curl -fLO https://github.com/ninja-build/ninja/releases/download/v1.7.1/ninja-win.zip
+    7z x ninja-win.zip -obin ninja.exe
+    ninja --version
+    rm ninja-win.zip
+    popd
+fi
+
+if [[ $COMPILER =~ msvc-16 && ! -d "_deps/boost_1_56_0-msvc-10.0-64" ]]; then
+    mkdir -p _deps
+    pushd _deps
     curl -fLO 'http://netix.dl.sourceforge.net/project/boost/boost-binaries/1.56.0/boost_1_56_0-msvc-10.0-64.exe'
 
     # Only extract headers and libraries so it completes quicker. (It's not
@@ -22,7 +40,7 @@ if [[ ! -d "_deps/boost_1_56_0-msvc-10.0-64" ]]; then
     # Try to keep under Appveyor cache limit by removing anything we don't need.
     # - documentation and source files
     # - dlls and import libs
-    # - remove libs linked against static CRt
+    # - remove libs linked against static CRT
     pushd boost_1_56_0-msvc-10.0-64
     rm -fr doc libs tools
     rm -fr lib64-msvc-10.0/boost*
@@ -30,6 +48,4 @@ if [[ ! -d "_deps/boost_1_56_0-msvc-10.0-64" ]]; then
     popd
 
     popd
-
-    set +o xtrace
 fi
